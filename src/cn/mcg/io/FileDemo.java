398802eac4a5ef6e1ec1ce6dfb2d7053a7025a13
maochunguang <maochunguang@xingshulin.com>
2017-08-10 00:43:19 +0800

    transfer GBK to utf8

---
+++ src/cn/mcg/io/FileDemo.java
@@ -0,0 +1,188 @@
+package cn.mcg.io;
+
+import java.io.*;
+
+/**
+ * Created by mao on 2017/8/9.
+ */
+public class FileDemo {
+
+    public static void readFileByBytes(String fileName) {
+        // 一般先创建file对象
+        FileInputStream fileInput = null;
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            byte[] buffer = new byte[1024];
+            fileInput = new FileInputStream(file);
+            int byteread = 0;
+            // byteread表示一次读取到buffers中的数量。
+            while ((byteread = fileInput.read(buffer)) != -1) {
+                System.out.write(buffer, 0, byteread);
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+
+            try {
+                if (fileInput != null) {
+                    fileInput.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+
+    public static void readFileByChars(String fileName) {
+        FileReader reader = null;
+        try {
+            File file = new File(fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            reader = new FileReader(file);
+            char[] buffer = new char[1024];
+            int charread = 0;
+            while ((charread = reader.read(buffer)) != -1) {
+                System.out.print(buffer);
+            }
+        } catch (IOException e) {
+            // TODO: handle exception
+
+        } finally {
+
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void readByBufferedReader(String fileName) {
+        try {
+            File file = new File(fileName, "");
+
+            // 读取文件，并且以utf-8的形式写出去
+            BufferedReader bufread;
+            String read;
+            bufread = new BufferedReader(new FileReader(file));
+            while ((read = bufread.readLine()) != null) {
+                System.out.println(read);
+            }
+            bufread.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void writeByFileOutputStream(String path) {
+
+        FileOutputStream fop = null;
+        File file;
+        String content = "This is the text content";
+        try {
+            file = new File(path);
+            fop = new FileOutputStream(file);
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            // get the content in bytes
+            byte[] contentInBytes = content.getBytes();
+
+            fop.write(contentInBytes);
+            fop.flush();
+            fop.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fop != null) {
+                    fop.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void writeByFileReader(String path) {
+        try {
+            String data = " This content will append to the end of the file";
+
+            File file = new File(path);
+            // if file doesnt exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            // true = append file
+            FileWriter fileWritter = new FileWriter(file.getName(), true);
+            fileWritter.write(data);
+            fileWritter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void writeByBufferedReader(String path, String content) {
+        try {
+            File file = new File(path);
+            file.delete();
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.flush();
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        String path = "/Users/mac/workspace/java-demo/src/test.txt";
+
+        String content = readFileByEncode(path, "GBK");
+
+        writeByBufferedReader(path, new String(content.getBytes("UTF-8"), "UTF-8"));
+
+    }
+
+    public static String readFileByEncode(String path, String chatSet) throws Exception {
+        InputStream input = new FileInputStream(path);
+        InputStreamReader in = new InputStreamReader(input, chatSet);
+        BufferedReader reader = new BufferedReader(in);
+        StringBuffer sb = new StringBuffer();
+        String line = reader.readLine();
+        while (line != null) {
+            sb.append(line);
+            sb.append("\r\n");
+            line = reader.readLine();
+        }
+
+        return sb.toString();
+    }
+
+}
---
+++ src/cn/mcg/io/TransferProject.java
@@ -0,0 +1,69 @@
+package cn.mcg.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Author: mac
+ * Date: 2017/8/9
+ * Description: todo
+ */
+public class TransferProject {
+    public static void transferFile(String pathName, int depth) throws Exception {
+        File dirFile = new File(pathName);
+        if (!isValidFile(dirFile)) return;
+        for (int j = 0; j < depth; j++) {
+            System.out.print("  ");
+        }
+        System.out.print("|--");
+        System.out.println(dirFile.getName());
+        //获取此目录下的所有文件名与目录名
+        String[] fileList = dirFile.list();
+        int currentDepth = depth + 1;
+        for (int i = 0; i < fileList.length; i++) {
+            String string = fileList[i];
+            File file = new File(dirFile.getPath(), string);
+            String name = file.getName();
+            //如果是一个目录，搜索深度depth++，输出目录名后，进行递归
+            if (file.isDirectory()) {
+                //递归
+                transferFile(file.getCanonicalPath(), currentDepth);
+            } else {
+                if (name.contains(".java") || name.contains(".properties") || name.contains(".xml")) {
+                    readAndWrite(file);
+                }
+            }
+        }
+    }
+
+    private static boolean isValidFile(File dirFile) throws IOException {
+        if (dirFile.exists()) {
+            System.out.println("file exist");
+            return true;
+        }
+        if (dirFile.isDirectory()) {
+            if (dirFile.isFile()) {
+                System.out.println(dirFile.getCanonicalFile());
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private static void readAndWrite(File file) throws Exception {
+        List<String> str = FileDemo.readFileByEncode(file.getPath(), "GBK");
+        System.out.println(str);
+        StringBuffer sb = new StringBuffer();
+        for (int j = 0; j < str.size(); j++) {
+            sb.append(str.get(j));
+            sb.append("\r\n");
+        }
+        FileDemo.writeByBufferedReader(file.getPath(), new String(sb.toString().getBytes("UTF-8"), "UTF-8"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        String path = "/Users/mac/Downloads/unit06_jdbc/src";
+        transferFile(path, 1);
+    }
+}
