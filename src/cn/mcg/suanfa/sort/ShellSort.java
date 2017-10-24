a606d36d9f8af2865392ed1f02aef7f777967b9a
maochunguang <maochunguang@xingshulin.com>
2017-10-24 21:40:35 +0800

    新增希尔排序

--- src/cn/mcg/suanfa/sort/ShellSort.java
+++ src/cn/mcg/suanfa/sort/ShellSort.java
@@ -5,14 +5,22 @@ import java.util.Arrays;
 /**
  * Author: mac
  * Date: 2017/10/22
- * Description: 插入排序实现
+ * Description: todo
  */
-public class InsertSort {
-    public static void insertSort(int[] a, int n) {
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j > 0 && (a[j]<a[j-1]); j--) {
-                swap(a, j, j-1);
+public class ShellSort {
+    public static void shellSort(int[] a) {
+        int n = a.length;
+        int h = 1;
+        while (h < n/3) h = 3*h + 1;
+
+        while (h >= 1) {
+            // h-sort the array
+            for (int i = h; i < n; i++) {
+                for (int j = i; j >= h && (a[j]< a[j-h]); j -= h) {
+                    swap(a, j, j-h);
+                }
             }
+            h /= 3;
         }
     }
     private static void swap(int[] a, int i, int j) {
@@ -23,10 +31,8 @@ public class InsertSort {
 
     public static void main(String[] args) {
         int[] arr = {49, 38, 65, 97, 76, 13, 27, 4, 78, 34, 12, 64, 1, 8};
-        insertSort(arr, arr.length);
+        shellSort(arr);
         System.out.println("排序之后：");
         System.out.println(Arrays.toString(arr));
-
     }
-
 }
